1b0d0539119c1553f4bb6fca2c9523a778c01a4b
liverHawk <hawkyofkn5365@gmail.com>
2025-10-19 17:17:45 +0900

    test file update

--- test.py
+++ test.py
@@ -1,20 +1,20 @@
 import torch
 import os
 
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
 # Try each GPU individually
 for i in range(4):
     try:
         device = torch.device(f'cuda:{i}')
         print(f'Testing GPU {i}...')
-        os.environ['CUDA_VISIBLE_DEVICES'] = str(i)
-        torch.cuda.set_device(device)
+        # torch.cuda.set_device(device)
 
         # get GPU information (ex: temperature, utilization, memory)
         temperature = torch.cuda.temperature(i)
         utilization = torch.cuda.utilization(i)
-        memory = torch.cuda.memory_stats(i)
-        str_memory = ', '.join([f'{k}: {v}' for k, v in memory.items()])
-        print(f'GPU {i} - Temperature: {temperature}°C, Utilization: {utilization}%, Memory Stats: {str_memory}')
+        # memory = torch.cuda.memory_stats(i)
+        # str_memory = ', '.join([f'{k}: {v}' for k, v in memory.items()])
+        print(f'GPU {i} - Temperature: {temperature}°C, Utilization: {utilization}%')
 
         x = torch.tensor([1.0], device=device)
         print(f'✓ GPU {i} works!')
