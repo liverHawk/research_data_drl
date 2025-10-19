95fea435f10017f12a92b55833fb821e814234d2
SpeedyHorse <toshizemi@icloud.com>
2025-10-19 17:41:39 +0900

    Squashed commit of the following:
    commit 08a989020203cca2142c729698e2fbd27d233c08
    Author: liverHawk <hawkyofkn5365@gmail.com>
    Date:   Sun Oct 19 17:29:05 2025 +0900
    
        version done
    
    commit 167669e9d5096bae11a7af95096af9d3c8562dac
    Author: liverHawk <hawkyofkn5365@gmail.com>
    Date:   Sun Oct 19 17:28:03 2025 +0900
    
        done
    
    commit 6745c4f09946242b65ffcf3a6ed733f59161a2c0
    Author: liverHawk <hawkyofkn5365@gmail.com>
    Date:   Sun Oct 19 17:27:36 2025 +0900
    
        done
    
    commit 57d6998ba59548b44e50b6d43c866f65153321b6
    Merge: 5427cc9 cee5f34
    Author: liverHawk <hawkyofkn5365@gmail.com>
    Date:   Sun Oct 19 17:26:12 2025 +0900
    
        Merge remote-tracking branch 'origin/desktop' into gpu
    
    commit 5427cc958afa2f04fa3d038a08a81fa56b45c826
    Author: liverHawk <hawkyofkn5365@gmail.com>
    Date:   Sun Oct 19 17:22:44 2025 +0900
    
        tmp
    
    commit 5b4e8002715a841667c6d2c4ba77137bd841ef38
    Author: liverHawk <hawkyofkn5365@gmail.com>
    Date:   Sun Oct 19 17:20:12 2025 +0900
    
        typo
    
    commit 1b0d0539119c1553f4bb6fca2c9523a778c01a4b
    Author: liverHawk <hawkyofkn5365@gmail.com>
    Date:   Sun Oct 19 17:17:45 2025 +0900
    
        test file update
    
    commit 3cd79eefe47d57693e13a32e14712338ec4105ab
    Author: liverHawk <hawkyofkn5365@gmail.com>
    Date:   Fri Oct 17 12:43:17 2025 +0900
    
        filter label

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
 import torch
 import os
 
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
 # Try each GPU individually
-print(f'Testing {torch.cuda.device_count()} GPUs...')
-for i in range(torch.cuda.device_count()):
+for i in range(4):
     try:
         device = torch.device(f'cuda:{i}')
         print(f'Testing GPU {i}...')
+        # torch.cuda.set_device(device)
+
+        # get GPU information (ex: temperature, utilization, memory)
+        temperature = torch.cuda.temperature(i)
+        utilization = torch.cuda.utilization(i)
+        # memory = torch.cuda.memory_stats(i)
+        # str_memory = ', '.join([f'{k}: {v}' for k, v in memory.items()])
+        print(f'GPU {i} - Temperature: {temperature}°C, Utilization: {utilization}%')
+
         x = torch.tensor([1.0], device=device)
         print(f'✓ GPU {i} works!')
         x = None
